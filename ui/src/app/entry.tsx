4799e286cb91bf70e99bf82fd42cbac4b35c57da
alexphloyd <alexphloyd@gmail.com>
2024-05-26 15:00:41 +0200

    incapsulate auth model listeners

--- ui/src/app/entry.tsx
+++ ui/src/app/entry.tsx
@@ -5,18 +5,11 @@ import '@mantine/core/styles/Notification.css';
 import '@mantine/core/styles/Tabs.css';
 import '@mantine/core/styles/global.css';
 import '@mantine/notifications/styles.css';
-import { messageBuilder } from 'core/src/infrastructure/channel-messaging/messages';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { ComposedApp } from './composed-app';
 import './styles/index.css';
-
-window.addEventListener('online', () => {
-    navigator.serviceWorker.controller?.postMessage(
-        messageBuilder.NETWORK_STATE_CHANGED('online')
-    );
-});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
--- ui/src/features/auth/model/initiate.ts
+++ ui/src/features/auth/model/initiate.ts
@@ -1,5 +1,13 @@
+import { messageBuilder } from 'core/src/infrastructure/channel-messaging/messages';
+
 import { defineSession } from './effects/define-session';
 
 export function initiate(dispatch: Dispatch) {
     dispatch(defineSession());
+
+    window.addEventListener('online', () => {
+        navigator.serviceWorker.controller?.postMessage(
+            messageBuilder.NETWORK_STATE_CHANGED('online')
+        );
+    });
 }
