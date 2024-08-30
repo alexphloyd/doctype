dcdd8e6a1e5635c2d74c05e635e78a553ac9d8e3
Alex Borysov <alexphloyd@gmail.com>
2024-08-31 00:02:24 +0200

    add code-block

--- ui/src/core/shared/ui/editor/extensions.config.ts
+++ ui/src/core/shared/ui/editor/extensions.config.ts
@@ -1,10 +1,9 @@
+import CodeBlock from '@tiptap/extension-code-block';
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
-import { Color } from '@tiptap/extension-color';
 import Document from '@tiptap/extension-document';
-import ListItem from '@tiptap/extension-list-item';
 import Paragraph from '@tiptap/extension-paragraph';
 import Text from '@tiptap/extension-text';
-import TextStyle from '@tiptap/extension-text-style';
+import { ReactNodeViewRenderer } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import css from 'highlight.js/lib/languages/css';
 import go from 'highlight.js/lib/languages/go';
@@ -13,6 +12,8 @@ import rust from 'highlight.js/lib/languages/rust';
 import ts from 'highlight.js/lib/languages/typescript';
 import html from 'highlight.js/lib/languages/xml';
 import { all, createLowlight } from 'lowlight';
+
+import { CodeBlockExtended } from './extensions/code-block';
 
 const lowlight = createLowlight(all);
 
@@ -24,21 +25,8 @@ lowlight.register('rust', rust);
 lowlight.register('go', go);
 
 export const extensions = [
-  Color.configure({ types: [TextStyle.name, ListItem.name] }),
-  TextStyle.configure(),
-  StarterKit.configure({
-    bulletList: {
-      keepMarks: true,
-      keepAttributes: false,
-    },
-    orderedList: {
-      keepMarks: true,
-      keepAttributes: false,
-    },
-  }),
-  Document,
-  Paragraph,
-  Text,
+  StarterKit.configure({ codeBlock: false }),
+  CodeBlockExtended,
   CodeBlockLowlight.configure({
     lowlight,
   }),
---
+++ ui/src/core/shared/ui/editor/extensions/code-block.tsx
@@ -0,0 +1,40 @@
+import { ActionIcon, CopyButton } from '@mantine/core';
+import { CheckIcon, CopyIcon } from '@radix-ui/react-icons';
+import CodeBlock from '@tiptap/extension-code-block';
+import {
+  NodeViewWrapper,
+  NodeViewContent,
+  ReactNodeViewRenderer,
+  type NodeViewProps,
+} from '@tiptap/react';
+
+export const CodeBlockExtended = CodeBlock.extend({
+  addNodeView() {
+    return ReactNodeViewRenderer(CodeBlockComponent);
+  },
+});
+
+function CodeBlockComponent(props: NodeViewProps) {
+  return (
+    <NodeViewWrapper as="div">
+      <div className="relative">
+        <CopyButton value={props.node.textContent} timeout={2000}>
+          {({ copied, copy }) => (
+            <ActionIcon
+              radius="sm"
+              color={copied ? 'teal' : 'gray'}
+              variant="transparent"
+              onClick={copy}
+              className="absolute top-[4px] right-[4px]"
+            >
+              {copied ? <CheckIcon /> : <CopyIcon />}
+            </ActionIcon>
+          )}
+        </CopyButton>
+        <pre>
+          <NodeViewContent as="code" />
+        </pre>
+      </div>
+    </NodeViewWrapper>
+  );
+}
