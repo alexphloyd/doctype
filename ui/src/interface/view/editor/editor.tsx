9bb6269287cfff942c3227e45b90a50dadba5d6b
Alex Borysov <alexphloyd@gmail.com>
2024-10-01 12:35:13 +0200

    feat: escape shortcut

--- ui/src/interface/view/editor/editor.tsx
+++ ui/src/interface/view/editor/editor.tsx
@@ -2,6 +2,7 @@ import { EditorContent, type EditorEvents, useEditor } from '@tiptap/react';
 import { observer } from 'mobx-react-lite';
 import { useCallback, useEffect } from 'react';
 import { DocumentSourceModel } from '~/interface/application/document/source/model';
+import { router } from '~/interface/kernel/router/mod.router';
 import { debounce } from '~/interface/shared/lib/debounce';
 
 import { extensions } from './extensions/extensions.config';
@@ -29,6 +30,19 @@ export const EditorView = observer(({ documentSourceModel }: Props) => {
   useEffect(() => {
     editor?.commands.setContent(documentSourceModel.source);
   }, [documentSourceModel.source]);
+
+  useEffect(() => {
+    const handleEscape = (event: KeyboardEvent) => {
+      if (event.code === 'Escape') {
+        router.navigate('/');
+      }
+    };
+
+    window.addEventListener('keydown', handleEscape);
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+    };
+  }, []);
 
   if (documentSourceModel.init.meta.status !== 'fulfilled') {
     return null;
