5392680809ac862c163575015226dfb308329a4e
Alex Borysov <alexphloyd@gmail.com>
2024-10-05 20:08:50 +0200

    fix: caching
    f

--- ui/src/service-worker/infrastructure/cache/precache.ts
+++ ui/src/service-worker/infrastructure/cache/precache.ts
@@ -3,8 +3,12 @@ import { registerRoute } from 'workbox-routing';
 import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
 
 export function precacheAndServeAssets() {
+  const origin = self.location.origin;
+
   registerRoute(
-    ({ request }) => request.destination === 'document' || request.mode === 'navigate',
+    ({ request }) =>
+      (request.destination === 'document' || request.mode === 'navigate') &&
+      request.url.startsWith(origin),
     new NetworkFirst({
       cacheName: 'html',
     })
@@ -15,10 +19,11 @@ export function precacheAndServeAssets() {
         request.destination === 'script' && request.url.includes('assets');
 
       return (
-        isApplicationAsset ||
-        ['style', 'font', 'image'].includes(request.destination) ||
-        request.url.endsWith('.svg') ||
-        request.url.includes('manifest')
+        (isApplicationAsset ||
+          ['style', 'font', 'image'].includes(request.destination) ||
+          request.url.endsWith('.svg') ||
+          request.url.includes('manifest')) &&
+        request.url.startsWith(origin)
       );
     },
     new StaleWhileRevalidate({
